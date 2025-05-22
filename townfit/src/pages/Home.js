1e6ee528d878133c3862f2bd0aadde464def44d4
Peace (PC) <cph0221@ajou.ac.kr>
2025-05-22 19:20:45 +0900

    feat: initialize React project

---
+++ townfit/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to TownFit</h1>
+      <p>Your fitness journey starts here!</p>
+    </div>
+  );
+}
+export default Home;
