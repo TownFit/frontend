5784866b350752029ed6f8f1d32548c0664777c2
Peace (PC) <cph0221@ajou.ac.kr>
2025-06-04 11:13:24 +0900

    feat: API Survey GET add

--- townfit/src/components/survey/SurveySidebar.js
+++ townfit/src/components/survey/SurveySidebar.js
@@ -2,7 +2,7 @@ import Button from "../common/Button";
 import SubTitle from "../common/SubTitle";
 import TextBox from "./TextBox";
 import SelectBox from "./SelectBox";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import pageState from "../../stores/states";
 import axios from "axios";
 
@@ -11,6 +11,23 @@ function SurveySidebar() {
     const [text, setText] = useState("");
     const options = ["반려동물", "아이", "학생", "노인"];
     const { goToSuggestion } = pageState();
+
+    // 1. 마운트 시 GET API 호출
+    useEffect(() => {
+        async function checkSurvey() {
+            try {
+                const res = await axios.get("https://mytownfit.com/survey/has-history");
+                if (res.data === true || res.data.has_history === true) {
+                    goToSuggestion();
+                } else {
+                    alert("history가 없습니다.")
+                }
+            } catch (error) {
+                alert("설문 조회에 실패했습니다.");
+            }
+        }
+        checkSurvey();
+    }, []);
 
     const handleGoToSuggestion = async () => {
         try {
